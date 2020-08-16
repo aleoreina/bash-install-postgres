fedef68c681079b4cf1997974d0e906226d83d49
Ángel <aleoreina@gmail.com>
2020-08-16 09:22:34 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 sudo apt-get install wget ca-certificates -y
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install postgresql postgresql-contrib
+sudo apt-get install postgresql postgresql-contrib -y
